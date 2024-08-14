0a3dfc7e51d443fde7e6757f9f23d2f5f46fbd8b
HarryStemwork <162728191+HarryStemwork@users.noreply.github.com>
2024-08-14 23:06:10 +0800

    Create countingWord.py

---
+++ countingWord.py
@@ -0,0 +1,43 @@
+song = """Old MacDonald had a farm,
+E I E I O
+And on that farm he had a cow,
+E I E I O
+With a moo moo here and a moo moo there,
+Here a moo, there a moo, everywhere a moo moo.
+Old MacDonald had a farm,
+E I E I O
+Old MacDonald had a farm,
+E I E I O
+And on that farm he had a duck,
+E I E I O
+With a quack quack here and a quack quack there,
+Here a quack, there a quack, everywhere a quack quack.
+Old MacDonald had a farm,
+E I E I O
+Old MacDonald had a farm,
+E I E I O
+And on that farm he had a horse,
+E I E I O
+With a neigh neigh here and a neigh neigh there,
+Here a neigh, there a neigh, everywhere a neigh neigh.
+Old MacDonald had a farm,
+E I E I O
+Old MacDonald had a farm,
+E I E I O
+And on that farm he had a pig,
+E I E I O
+With an oink oink here and an oink oink there,
+Here an oink, there an oink, everywhere an oink oink.
+Old MacDonald had a farm,
+E I E I O
+Old MacDonald had a farm,
+E I E I O"""
+
+split_word = #how to split the word
+word_count = #how to count the list
+word_input = input("Enter a word: ")
+check_word = #how to count the word in a list #extra
+
+print(f"Song list: {split_word}")
+print(f"Word count: {word_count}")
+print(f"{word_input} have repeated {check_word} time")
